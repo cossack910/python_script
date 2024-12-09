c8218204f3e786e45574e129af2794d0596f4e05
cossack910 <maris160zhk@gmail.com>
2024-12-10 04:56:34 +0900

    b

--- typing.py
+++ typing.py
@@ -1,7 +1,9 @@
 import random
+import time
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+start = time.time()
 cnt = 0
 while True:
     word = ''.join([alphabet[random.randint(0, 25)] for n in range(random.randint(5, 20))])
@@ -10,5 +12,7 @@ while True:
     if word == input_word:
         cnt += 1
     if cnt >= 30:
+        elapsed_time = time.time() - start
         print("clear!!!")
+        print(f"クリア時間: {elapsed_time:.2f} 秒")
         break
