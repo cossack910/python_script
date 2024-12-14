d34c9cd750e91e77c0c9060ed2ec5e9b82ff4294
cossack910 <maris160zhk@gmail.com>
2024-12-14 20:58:27 +0900

    ok

--- typing.py
+++ typing.py
@@ -3,18 +3,16 @@ import time
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-start = time.time()
-cnt = 0
 word_cnt = 0
+start_time = time.time()
+
 while True:
     word = ''.join([alphabet[random.randint(0, 25)] for n in range(random.randint(5, 20))])
     print(word)
-    word_cnt += len(word)
-    input_word = input()
-    if word == input_word:
-        cnt += 1
-    if cnt >= 30:
-        elapsed_time = time.time() - start
+    if word == input():
+        word_cnt += len(word)
+    if word_cnt >= 400:
+        elapsed_time = time.time() - start_time
         print("clear!!!")
         print(f"クリア時間: {elapsed_time:.2f} 秒")
         print(f"クリア文字数: {word_cnt}")
