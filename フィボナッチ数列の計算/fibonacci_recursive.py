7832a7f228b52366d40745cb0be68f71aedb12c9
cossack910 <maris160zhk@gmail.com>
2025-01-03 04:07:20 +0900

    リファクタ

--- フィボナッチ数列の計算/fibonacci_recursive.py
+++ フィボナッチ数列の計算/fibonacci_recursive.py
@@ -1,9 +1,8 @@
-def fibonacci_recursive(end_num: int, numbers: list = [0, 1], current_num: int = 0):
+def fibonacci_recursive(end_num: int, a = 0, b = 1, current_num = 0):
     if current_num == end_num:
-        print(numbers[end_num])
+        print(a)
         return True
-    numbers.append(numbers[current_num] + numbers[current_num + 1])
-    fibonacci_recursive(end_num, numbers, current_num + 1)
+    fibonacci_recursive(end_num, b, a + b, current_num + 1)
 
 end_num = int(input("整数を入力してください :"))
 fibonacci_recursive(end_num)
